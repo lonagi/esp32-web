5386f5247494b21d3ae700e28575a46b48c86651
Sasha <lonagi22@gmail.com>
2020-12-05 00:14:07 +0200

    variables

--- src/firmware.cpp
+++ src/firmware.cpp
@@ -1,7 +1,13 @@
 const char* ssid = "REPLACE_WITH_YOUR_SSID";
 const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+WiFiServer server(80);
+String header;
+String output26State = "off";
+String output27State = "off";
 const int output26 = 26;
 const int output27 = 27;
+unsigned long currentTime = millis();
+unsigned long previousTime = 0; 
 const long timeoutTime = 2000;
   // Initialize the output variables as outputs
   pinMode(output26, OUTPUT);

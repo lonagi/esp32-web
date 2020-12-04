6ace36ddca467ec9876ba64030ed97712cdecf33
Sasha <lonagi22@gmail.com>
2020-12-04 23:13:16 +0200

    consts

---
+++ src/firmware.cpp
@@ -0,0 +1,5 @@
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+const int output26 = 26;
+const int output27 = 27;
+const long timeoutTime = 2000;

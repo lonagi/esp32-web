9ba1476c8cf41c03d41ea8ae19e0ccb50cceed1e
Sasha <lonagi22@gmail.com>
2020-12-05 00:14:48 +0200

    setup WiFi

--- src/firmware.cpp
+++ src/firmware.cpp
@@ -19,3 +19,14 @@ void setup() {
   digitalWrite(output27, LOW);
   Serial.print("Connecting to ");
   Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}

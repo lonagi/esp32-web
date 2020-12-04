7c5a5411353979f5398c4ae28a44011a08ddf8c5
Sasha <lonagi22@gmail.com>
2020-12-05 00:13:50 +0200

    pinModes for outputs

--- src/firmware.cpp
+++ src/firmware.cpp
@@ -3,3 +3,9 @@ const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 const int output26 = 26;
 const int output27 = 27;
 const long timeoutTime = 2000;
+  // Initialize the output variables as outputs
+  pinMode(output26, OUTPUT);
+  pinMode(output27, OUTPUT);
+  // Set outputs to LOW
+  digitalWrite(output26, LOW);
+  digitalWrite(output27, LOW);

b1e9ef682a50c6726e91adcaf061fd682ddc9f86
Sasha <lonagi22@gmail.com>
2020-12-05 23:00:47 +0200

    comments

--- src/firmware.cpp
+++ src/firmware.cpp
@@ -1,14 +1,31 @@
+// Load Wi-Fi library
+#include <WiFi.h>
+
+// Replace with your network credentials
 const char* ssid = "REPLACE_WITH_YOUR_SSID";
 const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+// Set web server port number to 80
 WiFiServer server(80);
+
+// Variable to store the HTTP request
 String header;
+
+// Auxiliar variables to store the current output state
 String output26State = "off";
 String output27State = "off";
+
+// Assign output variables to GPIO pins
 const int output26 = 26;
 const int output27 = 27;
+
+// Current time
 unsigned long currentTime = millis();
+// Previous time
 unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
+
 void setup() {
   Serial.begin(115200);
   // Initialize the output variables as outputs
@@ -17,6 +34,8 @@ void setup() {
   // Set outputs to LOW
   digitalWrite(output26, LOW);
   digitalWrite(output27, LOW);
+
+  // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -24,9 +43,11 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
+  // Print local IP address and start web server
   Serial.println("");
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   server.begin();
 }
+

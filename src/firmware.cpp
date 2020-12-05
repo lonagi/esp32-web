b9041cde6d53ec76278d4eb1a1c790020d8d0125
Sasha <lonagi22@gmail.com>
2020-12-05 23:05:19 +0200

    HTML

--- src/firmware.cpp
+++ src/firmware.cpp
@@ -104,4 +104,49 @@ void loop(){
               digitalWrite(output27, LOW);
             }
             
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html {display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: yellow; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: grey;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>ESP32 Web Server</h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 26  
+            client.println("<p>GPIO 26 - State " + output26State + "</p>");
+            // If the output26State is off, it displays the ON button       
+            if (output26State=="off")
+              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+            else
+              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
+                           
+            // Display current state, and ON/OFF buttons for GPIO 27  
+            client.println("<p>GPIO 27 - State " + output27State + "</p>");
+            // If the output27State is off, it displays the ON button       
+            if (output27State=="off")
+              client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
+            else
+              client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
+            
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else
+            currentLine = "";
+        } else if (c != '\r') 
+
+          // if you got anything else but a carriage return character,
+          // add it to the end of the currentLine
+          currentLine += c;      
+      }
+    }
 }
